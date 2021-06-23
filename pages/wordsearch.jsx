974f7b56b0c64076340eeffef7f46b2ccc2106c1
shaun choy <devshaun.c@gmail.com>
2021-06-23 22:32:42 +0800

    test form on mobile

--- pages/wordsearch.jsx
+++ pages/wordsearch.jsx
@@ -163,6 +163,12 @@ const Vocabulary = (props) => {
         subtitle="[ 語彙 ] ごい"
         description="Simply search for Japanese words in our ever-growing library."
       />
+
+      <form onSubmit={() => alert("Clicked")}>
+        <input style={{ border: "1px solid black" }} />
+        <button type="submit">Enter</button>
+      </form>
+
       <form onSubmit={handleSearch}>
         <Box display="flex" flexDir="column" alignItems="center">
           <SearchBox
