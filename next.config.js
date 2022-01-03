0205121e9d249e793d4e84ce1409d8a3752a752b
shaun choy <wai-shaun.choy@siemens.com>
2022-01-03 12:05:59 +0800

    clean-up files

--- next.config.js
+++ next.config.js
@@ -3,34 +3,8 @@ const withImages = require("next-images");
 
 const nextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com"],
     disableStaticImages: true,
   },
 };
 
 module.exports = withPlugins([[withImages]], nextConfig);
-
-// module.exports = withPlugins([[withImages]], {
-//   webpack(config, options) {
-//     const { isServer } = options;
-//     config.module.rules.push({
-//       test: /\.(ogg|mp3|wav|mpe?g)$/i,
-//       exclude: config.exclude,
-//       use: [
-//         {
-//           loader: require.resolve("url-loader"),
-//           options: {
-//             limit: config.inlineImageLimit,
-//             fallback: require.resolve("file-loader"),
-//             publicPath: `${config.assetPrefix}/_next/static/images/`,
-//             outputPath: `${isServer ? "../" : ""}static/images/`,
-//             name: "[name]-[hash].[ext]",
-//             esModule: config.esModule || false,
-//           },
-//         },
-//       ],
-//     });
-
-//     return config;
-//   },
-// });
